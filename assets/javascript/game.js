7cc87fe917306ec012dd7e13656ddbe36ac6dbe6
Aashish Panvelkar <Aashish Panvelkar>
2017-02-11 19:06:23 -0500

    Updating before working on it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,28 @@
-function randomNumberGenerator(){
-	var randomNumber = Math.floor(Math.random() *120 + 19);
+function randomNumberGenerator() {
+
+    var randomNumber = Math.floor(Math.random() * 120 + 19);
 };
 
-$(document).ready(function() {
-        // This will choose my random num
-        var randomNumber = Math.floor(Math.random() * 101 + 19);
-        // This pushes my random num in to span located in div randomNumberDiv
-        $("#randomNumberDiv").text(randomNumber);
+// This will choose my random num
+var randomNumber = Math.floor(Math.random() * 101 + 19);
+// This pushes my random num in to randomNumberDiv
+$("#randomNumberDiv").text(randomNumber);
 
 
+$("#buttonOne").on("click", function() {
+    var buttonOneRandomValue = Math.floor(Math.random() * 12 + 1);
+    console.log(buttonOneRandomValue);
+});
+$("#buttonTwo").on("click", function() {
+    var buttonTwoRandomValue = Math.floor(Math.random() * 12 + 1);
+    console.log(buttonTwoRandomValue);
+});
+$("#buttonThree").on("click", function() {
+    var buttonThreeRandomValue = Math.floor(Math.random() * 12 + 1);
+    console.log(buttonThreeRandomValue);
+});
+$("#buttonFour").on("click", function() {
+    var buttonFourRandomValue = Math.floor(Math.random() * 12 + 1);
+    console.log(buttonFourRandomValue);
+});
 
-    })
